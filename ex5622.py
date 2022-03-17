4bc63894c232041b70b6d41f27ab4c7007c7ada1
eunseo <80867941+leeeunseo912@users.noreply.github.com>
2022-03-17 15:38:53 +0900

    ex5622

---
+++ ex5622.py
@@ -0,0 +1,9 @@
+dial = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
+word = input()
+num = 0
+for i in range(len(word)):
+    for j in dial:
+        if word[i] in j:
+            num += dial.index(j) +3
+
+print(num)
