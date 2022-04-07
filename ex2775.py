da2dd329d7a1cfd32d3a5731513b957c103bcd12
eunseo <80867941+leeeunseo912@users.noreply.github.com>
2022-04-07 23:38:57 +0900

    ex2775

---
+++ ex2775.py
@@ -0,0 +1,12 @@
+test = int(input())
+
+for i in range(test):
+    k = int(input())
+    n = int(input())
+    people = [i for i in range(1,n+1)]
+
+    for i in range(k):
+        for j in range(1, n):
+            people[j] += people[j-1]
+
+    print(people[-1])
