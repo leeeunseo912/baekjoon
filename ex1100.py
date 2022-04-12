ab76e664ac5278ec1d569e6abb517b682090f70a
eunseo <80867941+leeeunseo912@users.noreply.github.com>
2022-04-12 19:50:14 +0900

    ex1100

---
+++ ex1100.py
@@ -0,0 +1,14 @@
+chess = []
+count = 0
+
+for _ in range(8):
+    chess.append(input())
+
+for i in range(8):
+    for j in range(8):
+        if i % 2 == 1 and j % 2 == 1 and chess[i][j] == "F":
+            count += 1
+        if i % 2 == 0 and j % 2 == 0 and chess[i][j] == "F":
+            count += 1
+  
+print(count)
