4d65f53f23fe54aee43658e8ea8cde24c9ec696c
eunseo <80867941+leeeunseo912@users.noreply.github.com>
2022-07-29 16:49:33 +0900

    ex1978

--- ex1978.py
+++ ex1978.py
@@ -4,17 +4,18 @@ input = sys.stdin.readline
 n = int(input())
 num_list = list(map(int,input().split()))
 
-error = 0
+
 prime = 0
-print(num_list)
+# print(num_list)
 
 for i in num_list:
+    error = 0
     if i > 1:
-        for j in range(2, i):
+        for j in range(2, i+1):
             if i % j == 0:
                 error += 1
-                print("error" , error)
-        if error == 0:
+                # print("error" , error)
+        if error == 1:
             prime += 1
     
 print(prime)
