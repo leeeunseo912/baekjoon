0209a85fca5539bd3140dda19765424b2c9a2332
eunseo <80867941+leeeunseo912@users.noreply.github.com>
2022-08-21 19:30:06 +0900

    ex2477

--- ex2477.py
+++ ex2477.py
@@ -16,15 +16,15 @@ for i in range(6):
         height.append(len)
         total.append(len)
 
-# print(width)
-# print(height)
+print(total)
 big_area = (max(width)*max(height))
 
 maxhidx = total.index(max(height))
 maxwidx = total.index(max(width))
-
+print(maxhidx, maxwidx)
 small1 = abs(total[maxhidx-1] - total[(maxhidx-5 if maxhidx == 5 else maxhidx +1)])
 small2 = abs(total[maxwidx-1] - total[(maxwidx-5 if maxwidx == 5 else maxwidx +1)])
+print(small1, small2)
 
 area = big_area - (small1 * small2)
 print(area * num)
