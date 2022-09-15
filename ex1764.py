faef14846f62fdab1005b9b9c6cd93049b4c9137
eunseo <80867941+leeeunseo912@users.noreply.github.com>
2022-09-15 19:01:06 +0900

    ex1764

---
+++ ex1764.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split(' '))
+hear = set()
+
+for i in range(n):
+    name = input()
+    hear.add(name)
+
+see = set()
+result = []
+
+for i in range(m):
+    name = input()
+    if name in hear:
+        result.append(name)
+       
+
+print(len(result))
+result.sort()
+for i in result:
+    print(i, end="")
+
